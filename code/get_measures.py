d9ed8e49a32eadef582925557f87895565a2b03e
SarahMorgan <sarah_morgan@btinternet.com>
2021-02-26 17:27:37 +0000

    Adapt scripts for general use

--- code/get_measures.py
+++ code/get_measures.py
@@ -24,13 +24,13 @@ from tangent_meas import *
 from spider_plot import *
 
 # import file that contains participant's speech excerpt:
-speechfilename = 'c:\\Users\\sem91\\Documents\\Research\\speech\\github_code\\speech_example1.txt'
+speechfilename = 'c:\\Users\\sem91\\Documents\\GitHub\\NLP_psychosis\\examples\\speech_example2.txt'
 speechfile = open(speechfilename, 'rt')
 speechtext = speechfile.read()
 speechfile.close()
 
 # import file that contains 'ground truth' picture description:
-picfilename = 'c:\\Users\\sem91\\Documents\\Research\\speech\\TAT\\picture7.txt'
+picfilename = 'c:\\Users\\sem91\\Documents\\GitHub\\NLP_psychosis\\examples\\TAT\\picture7.txt'
 picfile = open(picfilename, 'rt')
 pictext = picfile.read()
 picfile.close()
@@ -57,5 +57,5 @@ s_vec=np.array([36,4,3.6,0.2337,0.5482,-0.0758,0.2929])
 e_vec=np.array([194,21,38.5,0.6824,1,0.0294,0.5424])
 
 # plot spider plot:
-output_file='spider_result.png' # filename for output spider plot
+output_file='c:\\Users\\sem91\\Documents\\GitHub\\NLP_psychosis\\examples\\spider_result2.png' # filename for output spider plot
 plot_spider(result,s_vec,e_vec,spoke_titles,output_file)
